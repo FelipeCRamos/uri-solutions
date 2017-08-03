771e06f98b50acf4a0f09971e61f16fe48492f04
FelipeCRamos <felipe.da.costa_10@hotmail.com>
2017-08-03 02:04:46 -0300

    Deleting

--- Beginner Problems/1001.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-  int A, B = 0;
-  scanf("%i\n%i", &A, &B);
-  printf("X = %i\n", (A+B));
-  return 0;
-}
--- Beginner Problems/1078.c
+++
@@ -1,9 +0,0 @@
-#include<stdlib.h>
-#include<stdio.h>
-int main(){
-  int n, i;
-  scanf("%i", &n);
-  for(i=1; i<=10; i++){
-    printf("%i x %i = %i\n", i, n, (i*n));
-  }
-}
